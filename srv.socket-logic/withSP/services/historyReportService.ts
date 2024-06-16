68adcb922a5e19300dc71e776dbbb05dcb9b87cc
Den <selection4test@gmail.com>
2024-06-16 21:52:39 +0300

    logs updated

--- srv.socket-logic/withSP/services/historyReportService.ts
+++ srv.socket-logic/withSP/services/historyReportService.ts
@@ -3,9 +3,39 @@ import { getChannelName, getIsCorrectFormat, mws, state } from '~/srv.socket-log
 import { Socket } from 'socket.io'
 import { universalHttpClient } from '~/srv.utils/universalHttpClient'
 
-const getArrayFromSet = (set: Set<string>): string[] => Array.from(set)
-const getLogsAsMultilineText = (set: Set<string>): string => getArrayFromSet(set).map((msg, _i) => `• \`${msg}\``).join('\n')
-const getLogsAsSingleLineText = (set: Set<string>): string => getArrayFromSet(set).join(' // ')
+class Logger {
+  private logs: Set<string>
+  private globalCounter: number
+  private counterLimit: number
+  constructor({ counterLimit }: { counterLimit: number }) {
+    this.counterLimit = counterLimit
+    this.globalCounter = 0
+    this.logs = new Set<string>()
+  }
+  add({ message }: { message: string }) {
+    if (this.counterLimit > this.globalCounter) this.globalCounter += 1
+    else this.globalCounter = 1
+
+    this.logs.add(`${this.getZero2(this.globalCounter)}. ${message}`)
+  }
+  clear() {
+    this.logs.clear()
+  }
+  get msgs(): string[] {
+    return Array.from(this.logs)
+  }
+  get logsAsSingleLineText(): string {
+    return this.msgs.join(' // ')
+  }
+  get logsAsMultilineText(): string {
+    return this.msgs.join('\n')
+  }
+
+  getZero(n: number): string { return n < 10 ? `0${n}` : `${n}` }
+  getZero2(n: number): string { return n < 10 ? `00${n}` : n < 100 ? `0${n}` : `${n}` }
+}
+
+const logger = new Logger({ counterLimit: 10000 })
 
 export const historyReportService = ({
   ip,
@@ -28,24 +58,26 @@ export const historyReportService = ({
       console.log(incData)
       console.log('-- /EV')
 
-      // const resultMsgs = []
-      const log = new Set<string>()
-
-      if (!!e.reason) log.add(`[Version validation result] ok: ${String(e.ok)}; reason: ${e.reason || 'Empty'})`)
+      if (!!e.reason) logger.add({ message: `[Version validation result] ok: ${String(e.ok)}${!!e.reason ? `; ${e.reason}` : ''}` })
       
       if (e.ok) {
-        state.addReportToReestr({ roomId: incData.room, report: { ...incData, _ip: ip, _userAgent: userAgent, _clientReferer: clientReferer } })
-        io
-          .in(getChannelName(incData.room))
-          .emit(NEvent.ServerOutgoing.SP_TRADEIN_REPORT_EV, {
-            message: 'New report',
-            report: { ...incData, _ip: ip, _userAgent: userAgent, _clientReferer: clientReferer },
-          })
+        const validated = getIsCorrectFormat(incData)
+        logger.add({ message: `[Inc data validation result] ok: ${String(validated.ok)}${!!validated.reason ? `; ${validated.reason}` : ''}` })
+
         // -- NOTE: Report to Google Sheets
         const ts = new Date().getTime()
         try {
-          const validated = getIsCorrectFormat(incData)
-          log.add(`[Inc data validation result] ok: ${String(validated.ok)}; reason: ${validated.reason || 'Empty'})`)
+          const addToReestrResult = await state.addReportToReestr({ roomId: incData.room, report: { ...incData, _ip: ip, _userAgent: userAgent, _clientReferer: clientReferer } })
+          logger.add({ message: `[Add data to reestr result] isOk: ${String(addToReestrResult.isOk)}${!!addToReestrResult.message ? `; ${addToReestrResult.message}` : ''}` })
+          
+          if (addToReestrResult.isOk) io
+            .in(getChannelName(incData.room))
+            .emit(NEvent.ServerOutgoing.SP_TRADEIN_REPORT_EV, {
+              message: 'New report',
+              report: { ...incData, _ip: ip, _userAgent: userAgent, _clientReferer: clientReferer },
+            })
+          else throw new Error(addToReestrResult.message || 'No message')
+          
           if (validated.ok) {
             const result = await universalHttpClient.post(
               '/express-helper/sp/report/v2/offline-tradein/mtsmain2024/send',
@@ -70,16 +102,26 @@ export const historyReportService = ({
               },
             )
 
-            log.add(`[Result of sending to /express-helper/sp/report/v2/offline-tradein/mtsmain2024/send] isOk: ${String(result?.isOk)}; message: ${result.message || 'Empty'})`)
+            logger.add({
+              message: `[Result of sending to /express-helper/sp/report/v2/offline-tradein/mtsmain2024/send] isOk: ${String(result?.isOk)}${!!result?.message ? `; message: ${result.message}` : ''}`,
+            })
 
             if (!result?.isOk) {
-              log.add('[Cur result] Cant save data to Google Sheets (cache only)')
+              logger.add({ message: '[Cur result] Cant save data to Google Sheets (cache only)' })
               const ts = new Date().getTime()
               const tgNotifResult = await universalHttpClient.post(
                 'http://pravosleva.pro/tg-bot-2021/notify/kanban-2021/reminder/send',
                 {
                   resultId: ts,
-                  chat_id: 432590698, // NOTE: Den Pol
+
+                  // NOTE: Den Pol
+                  // chat_id: 432590698,
+
+                  // NOTE: SP Report group (Offline Trade-In)
+                  chat_id: -1002189284187,
+                  // message_thread_id: 1,
+                  // message_thread_id: 2,
+
                   ts,
                   eventCode: 'aux_service',
                   about: `⛔ ${incData.appVersion} (rep) -> withSP -> historyReportService -> e-helper: API ERR`,
@@ -87,34 +129,29 @@ export const historyReportService = ({
                     'Cant send to Google Sheets:',
                     `${result.message || 'No message'}`,
                     '',
-                    `\`IP: ${ip || 'No'}\``,
-                    `\`IMEI: ${incData.imei || 'No'}\``,
-                    `\`Client referer: ${clientReferer || 'No'}\``,
+                    `\`\`\`\nIP: ${ip || 'No'}\nIMEI: ${incData.imei || 'No'}\nClient referer: ${clientReferer || 'No'}\`\`\``,
                     '',
                     !!incData.stepDetails?.commentByUser
                       ? `\`\`\`\n${incData.stepDetails?.commentByUser}\`\`\``
                       : '(no comment)',
-                    // '',
-                    // 'Log:',
-                    // `\`\`\`\n${resultMsgs.map((msg, i) => `${i + 1}. ${msg}`).join('\n')}\`\`\``,
                   ].join('\n'),
                 },
               )
-              log.add(`[Send err tg notif] isOk: ${String(tgNotifResult?.isOk)}; message: ${tgNotifResult?.message || 'Empty'}`)
+              logger.add({ message: `[Send err tg notif] isOk: ${String(tgNotifResult?.isOk)}${!!tgNotifResult?.message ? `; message: ${tgNotifResult.message}` : ''}` })
               io.to(socket.id).emit(NEvent.ServerOutgoing.SP_MX_SERVER_ON_HISTORY_REPORT_ANSWER_ERR, {
-                _message: getLogsAsSingleLineText(log),
+                _message: logger.logsAsSingleLineText,
                 message: 'Сомнительно, но Ok',
                 result,
                 yourData: incData,
               })
-              log.add('[Final msg for client] Сомнительно, но Ok')
+              logger.add({ message: '[Final msg for client] Сомнительно, но Ok' })
             } else {
               if (!!result?.response?.id) googleSheetRowNumber = result.response.id
-              log.add('[Cur result] Data saved to Google Sheets')
-              const uiMsg = result.response.id ? `Ok #${result.response.id}` : 'Ok'
+              logger.add({ message: `[Cur result] Data saved to Google Sheets (${result.response?.id || 'No response.id'})` })
+              const uiMsg = result.response?.id ? `Ok #${result.response.id}` : 'Ok'
               if (typeof cb === 'function') cb({ message: uiMsg, ok: true })
               io.to(socket.id).emit(NEvent.ServerOutgoing.SP_MX_SERVER_ON_HISTORY_REPORT_ANSWER_OK, {
-                _message: getLogsAsSingleLineText(log),
+                _message: logger.logsAsSingleLineText,
                 message: uiMsg,
                 result,
               })
@@ -126,14 +163,22 @@ export const historyReportService = ({
           if (typeof cb === 'function') cb({ message, ok: false })
           io.to(socket.id).emit(NEvent.ServerOutgoing.SP_MX_SERVER_ON_HISTORY_REPORT_ANSWER_ERR, {
             message,
-            _message: getLogsAsSingleLineText(log),
+            _message: logger.logsAsSingleLineText,
             yourData: incData,
           })
           universalHttpClient.post(
             'http://pravosleva.pro/tg-bot-2021/notify/kanban-2021/reminder/send',
             {
               resultId: ts,
-              chat_id: 432590698, // NOTE: Den Pol
+              
+              // NOTE: Den Pol
+              // chat_id: 432590698,
+
+              // NOTE: SP Report group (Offline Trade-In)
+              chat_id: -1002189284187,
+              // message_thread_id: 1,
+              // message_thread_id: 2,
+
               ts,
               eventCode: 'aux_service',
               about: `⛔ ${incData.appVersion} -> withSP: ERR`,
@@ -141,12 +186,7 @@ export const historyReportService = ({
                 message,
                 'Не все пошло по плану. Скорее всего, решение вопроса все еще в кэше сервера',
                 '',
-                `\`IP: ${ip || 'No'}\``,
-                `\`IMEI: ${incData.imei || 'No'}\``,
-                `\`Client referer: ${clientReferer || 'No'}\``,
-                // '',
-                // 'Log:',
-                // `\`\`\`\n${resultMsgs.map((msg, _i) => `• ${msg}`).join('\n')}\`\`\``,
+                `\`\`\`\nIP: ${ip || 'No'}\nIMEI: ${incData.imei || 'No'}\nClient referer: ${clientReferer || 'No'}\`\`\``,
               ].join('\n'),
             },
           )
@@ -155,17 +195,25 @@ export const historyReportService = ({
             'http://pravosleva.pro/tg-bot-2021/notify/kanban-2021/reminder/send',
             {
               resultId: ts,
-              chat_id: 432590698, // NOTE: Den Pol
+              
+              // NOTE: Den Pol
+              // chat_id: 432590698,
+
+              // NOTE: SP Report group (Offline Trade-In)
+              chat_id: -1002189284187,
+              // message_thread_id: 1,
+              // message_thread_id: 2,
+
               ts,
               eventCode: 'aux_service',
               about: !!googleSheetRowNumber ? `#report${googleSheetRowNumber} Details` : 'Experience',
               targetMD: [
                 'Last report log:',
-                getLogsAsMultilineText(log),
+                `\`\`\`\n${logger.logsAsMultilineText}\`\`\``,
               ].join('\n'),
             },
           ).finally(() => {
-            log.clear()
+            logger.clear()
           })
         }
         // --
