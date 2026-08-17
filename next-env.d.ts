9e61a515f8278a20657a96807eab041c12f85651
Den Pol <Denis_Poltoratskiy@rgs.ru>
2026-08-17 05:31:34 +0300

    fix

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,3 +4,9 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+// Объявляем дикую карту (wildcard) для CSS модулей и side-effect импортов
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
