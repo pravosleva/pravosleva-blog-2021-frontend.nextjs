5d80e1bce680795bee85257a1ba671ec338987c7
Den Pol <Denis_Poltoratskiy@rgs.ru>
2026-08-18 12:39:36 +0300

    mini refactoring

--- src/components/ArticlesList/components/PagesGrid/GridItem.tsx
+++ src/components/ArticlesList/components/PagesGrid/GridItem.tsx
@@ -1,29 +1,15 @@
 import { memo } from 'react'
-// import { TCRMPage } from '~/store/reducers/crmPages'
-// import { useStyles } from './styles'
-// import {
-//   setIsModalOpened,
-//   loadPageData,
-// } from '~/actions'
-// import { useDispatch } from 'react-redux'
-// import { useCallback } from 'react'
 import { getNormalizedDate } from '~/utils/time-tools/timeConverter'
 import { TArticle } from '~/components/Article/types'
-// import { ThemedButton, EPartnerCode } from '~/common/material/ThemedButton'
-// import { useSpring, animated } from 'react-spring'
 import Link from '~/components/Link'
 import { slugMap } from '~/constants/blog/slugMap'
 import { Button } from '@mui/material'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
-// import Link from 'next/dist/client/link'
-// import PauseIcon from '@mui/icons-material/Pause'
-// import HourglassFullIcon from '@mui/icons-material/HourglassFull'
 
 type TProps = {
   article: TArticle;
 }
 
-// const defaultBgUrl = '/static/img/blog/coming-soon.avif'
 const defaultBgUrl = '/static/img/blog/coming-soon-v3.jpg'
 
 export const GridItem = memo(({ article }: TProps) => {
@@ -34,38 +20,25 @@ export const GridItem = memo(({ article }: TProps) => {
     // description,
     title,
   } = original
-  // const classes = useStyles()
   const url = bg?.src || defaultBgUrl
-  // const dispatch = useDispatch()
-  // const getProject = useCallback((id: string) => {
-  //   dispatch(setIsModalOpened(true))
-  //   dispatch(loadPageData(id))
-  // }, [dispatch])
 
   return (
     <div
-      // className={classes.gridItemBg}
       className='gridItemBg'
       style={{
         backgroundImage: `url(${url})`,
         filter: !!bg?.src ? 'none' : 'grayscale(100%)',
-        // opacity: !!bg?.src ? 1 : 0.5,
         fontFamily: 'Montserrat, system-ui',
       }}
     >
       <div className='gridItemBox'>
-        {/* NOTE: h3 className='truncate' */}
         <div className='gridItemTitle'><h3>{title}</h3></div>
         <div className='gridItemDescription'>{brief}</div>
         <div className='gridItemAction'>
           {
             slugMap.has(_id) ? (
               <div>
-                {/* <ThemedButton partnerCode={EPartnerCode.Yellow} size='small' onClick={() => getProject(id)} variant='outlined' color="primary">
-                  Read
-                </ThemedButton> */}
                 <Button
-                  // fullWidth
                   variant='contained'
                   color='primary'
                   component={Link}
--- src/components/ArticlesList/components/PagesGrid/PagesGrid.tsx
+++ src/components/ArticlesList/components/PagesGrid/PagesGrid.tsx
@@ -1,18 +1,12 @@
 import { memo } from 'react'
-// import { useStyles } from './styles'
 import { GridItem } from './GridItem'
 import { TArticle } from '~/components/Article/types'
-// import styles from './PagesGrid.module.scss'
-// import clsx from 'clsx' 
 
-export const PagesGrid = memo(({ articles }: { articles: TArticle[] }) => {
-  // const classes = useStyles()
-  
-  return (
+export const PagesGrid = memo(({ articles }: { articles: TArticle[] }) => (
     <div className='articleListWrapper'>
       {
         articles.map((article) => <GridItem key={article.original._id} article={article} />)
       }
     </div>
   )
-})
+)
