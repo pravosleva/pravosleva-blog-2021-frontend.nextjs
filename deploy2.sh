43ff260bbc5d382cdb4049d5e015bac40039c810
Den Pol <Denis_Poltoratskiy@rgs.ru>
2026-09-07 17:01:48 +0300

    fix

--- deploy2.sh
+++ deploy2.sh
@@ -2,32 +2,35 @@ source ./read-env.sh
 
 DEPLOY_HOST=$(read_env BASH_DEPLOY_HOST .env."$1")
 
-# Проверяем, удалось ли прочитать переменную, чтобы не деплоить в "никуда"
+# -- Проверяем, удалось ли прочитать переменную, чтобы не деплоить в "никуда"
 if [ -z "$DEPLOY_HOST" ]; then
   echo "❌ ERROR: DEPLOY_HOST is not defined for environment '$1'"
   exit 1
 fi
 
-# Базовый путь на удаленном сервере
+# -- Базовый путь на удаленном сервере
 REMOTE_ROOT=$(read_env BASH_REMOTE_ROOT .env."$1")
 
-# Формируем динамические пути на основе прочитанного
+# -- Формируем динамические пути на основе прочитанного
 deploy_path_build_dir="${DEPLOY_HOST}:${REMOTE_ROOT}/.next"
 deploy_path_public_dir="${DEPLOY_HOST}:${REMOTE_ROOT}/public"
-deploy_path_config_file="${DEPLOY_HOST}:${REMOTE_ROOT}/next.config.js"
-deploy_path_package_json_file="${DEPLOY_HOST}:${REMOTE_ROOT}/package.json"
-#deploy_path_server_dist_dir="${DEPLOY_HOST}:${REMOTE_ROOT}/server.dist"
+deploy_path_server_dist_dir="${DEPLOY_HOST}:${REMOTE_ROOT}/server.dist"
 #deploy_path_node_modules_dir="${DEPLOY_HOST}:${REMOTE_ROOT}/node_modules"
+#deploy_path_config_file="${DEPLOY_HOST}:${REMOTE_ROOT}/next.config.js"
+#deploy_path_package_json_file="${DEPLOY_HOST}:${REMOTE_ROOT}/package.json"
 
 echo '-- 🚄 DEPLOY STARTED' &&
 
+# -- Копирование директорий
 rsync -av --delete .next/ $deploy_path_build_dir &&
 rsync -av --delete public/ $deploy_path_public_dir &&
+rsync -av --delete server.dist/ $deploy_path_server_dist_dir &&
+#rsync -av --delete node_modules/ $deploy_path_node_modules_dir &&
 
-# Явное копирвание конфигов и прочего вспомогательного
-rsync -av next.config.js $deploy_path_config_file &&
-rsync -av package.json $deploy_path_package_json_file &&
-rsync -av .env."$1" "${DEPLOY_HOST}:${REMOTE_ROOT}/.env.$1" &&
+# -- Явное копирвание конфигов и прочего вспомогательного
+#rsync -av .env."$1" "${DEPLOY_HOST}:${REMOTE_ROOT}/.env.$1" &&
 rsync -av .env."$1" "${DEPLOY_HOST}:${REMOTE_ROOT}/.env.production" &&
+#rsync -av next.config.js $deploy_path_config_file &&
+#rsync -av package.json $deploy_path_package_json_file &&
 
 echo '-- 🏁 DEPLOY COMPLETED'
