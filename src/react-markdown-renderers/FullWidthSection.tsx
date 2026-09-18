1622732738ccb0b5af7a090b59b223c200de676a
Den Pol <Denis_Poltoratskiy@rgs.ru>
2026-09-18 13:13:54 +0300

    content updated

--- src/react-markdown-renderers/FullWidthSection.tsx
+++ src/react-markdown-renderers/FullWidthSection.tsx
@@ -1,9 +1,10 @@
 import { AuditListIntroSvg } from "~/components/special-svg-content/projects/audit-list/AuditListIntroSvg.v2"
 import { ReactiveEngineIntroSvg } from "~/components/special-svg-content/projects/reactive-engine/_tmp"
 import { RetroDiceIntroSvg } from "~/components/special-svg-content/projects/retro-dice/RetroDiceIntroSvg.v4"
+import { OnlineTradeinIntroSvg } from "~/components/special-svg-content/projects/tradein/OnlineTradeinIntroSvg"
 
 interface IProps {
-  contentCode: 'reactive-engine' | 'retro-dice' | 'audit-list'
+  contentCode: 'reactive-engine' | 'retro-dice' | 'audit-list' | 'online-tradein'
 }
 
 export const FullWidthSection = ({ contentCode }: IProps) => {
@@ -30,6 +31,15 @@ export const FullWidthSection = ({ contentCode }: IProps) => {
           style={{ width: '100%', marginBottom: '1.45rem' }}
         >
           <AuditListIntroSvg />
+        </div>
+        
+      )
+    case 'online-tradein':
+      return (
+        <div
+          style={{ width: '100%', marginBottom: '1.45rem' }}
+        >
+          <OnlineTradeinIntroSvg />
         </div>
         
       )
